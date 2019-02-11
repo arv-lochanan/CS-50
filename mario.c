97710857699d6ade89def42038e51e6745b10518
Lochanan <arv.lochanan@gmail.com>
2019-02-11 11:44:36 +0530

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+int main(void)
+{
+    int height;
+    loop:
+    printf("Please enter a height: ");
+    scanf("%d", &height);
+    if(height < 0 || height > 9)
+    {
+     goto loop;
+     } 
+    for (int i = height; i >= 1; i--)
+    {
+        for (int space = 1; space < i; space++)
+        {    
+          printf(" ");
+  	}       
+	for (int hash = height; hash >= i-1; hash--)
+	{         
+   	  printf("#");
+	}        
+	printf("\n");
+    }
+}
